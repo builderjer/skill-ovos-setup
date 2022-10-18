428b527e9e6982968b4cab43e59a44402420be64
Aditya Mehra <aix.m@outlook.com>
2022-10-19 10:09:22 +1030

    disable converse completely if using gui mode as its totally broken (#51)
    * disable converse completely as its a broken mess
    
    * use pairing mode check instead
    
    * additional fixes
    
    * fix missing syntax

--- __init__.py
+++ __init__.py
@@ -502,12 +502,13 @@ class PairingSkill(OVOSSkill):
 
     # voice events
     def converse(self, message):
-        if self.state != SetupState.INACTIVE or \
-                self.state != SetupState.FIRST_BOOT:
-            # capture all utterances until paired
-            # prompts from this skill are handled with get_response
-            return True
-        return False
+        if self.pairing_mode != PairingMode.GUI:
+            if self.state != SetupState.INACTIVE and \
+                self.state != SetupState.FINISHED:
+                # capture all utterances until paired
+                # prompts from this skill are handled with get_response
+                return True
+            return False
 
     @intent_handler(IntentBuilder("PairingIntent")
                     .require("pairing").require("device"))
